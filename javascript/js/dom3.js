360c477d5771e5f8bb5417302ef2dd946762d5ad
code8144 <dpeps456789@naver.com>
2023-03-16 17:49:33 +0900

    20230316 javascript

--- javascript/js/dom3.js
+++ javascript/js/dom3.js
@@ -13,7 +13,6 @@ addButton[0].onclick = () => {
         <li><span class="todo-content">${todoText[0].value}</span> <button class="ok-button">확인</button></li>
     `;
     statusList.push(false);
-    todoText.value = "";
 
     addEvent();
 }
@@ -35,33 +34,3 @@ function addEvent() {
         }
     }
 }
-
-// // DOM 요소를 가져옵니다.
-// const todoText = document.querySelector('.todo-text');
-// const addButton = document.querySelector('.add-button');
-// const todoList = document.querySelector('.todo-list');
-
-// // 추가 버튼에 클릭 이벤트 리스너를 추가합니다.
-// addButton.addEventListener('click', () => {
-//   const newTodo = document.createElement('li');
-//   newTodo.innerHTML = `
-//     <span class="todo-content">${todoText.value}</span>
-//     <button class="ok-button">확인</button>
-//   `;
-//   todoList.appendChild(newTodo);
-//   todoText.value = '';
-// });
-
-// // ul 요소에 이벤트 위임을 사용하여 확인 버튼 클릭 이벤트를 처리합니다.
-// todoList.addEventListener('click', (event) => {
-//   if (event.target.classList.contains('ok-button')) {
-//     const todoContent = event.target.previousElementSibling;
-//     if (todoContent.style.color === 'red') {
-//       todoContent.style.color = 'black';
-//       todoContent.style.textDecoration = 'none';
-//     } else {
-//       todoContent.style.color = 'red';
-//       todoContent.style.textDecoration = 'line-through';
-//     }
-//   }
-// });
--- javascript/js/dom4.js
+++ javascript/js/dom4.js
@@ -1,15 +1,9 @@
-/**
- *  1. 추가버튼 클릭시 input에 들어있는 value의 값을 list에 추가
- *  2. 확인버튼 클릭시 todo-content의 색상을 red로 변경, 취소선 적용
- *  3. 확인버튼 다시 클릭시 todo-content의 색상을 black으로 변경, 취소선 미적용
- */
-
 const addButton = document.getElementsByClassName("add-button");
 addButton[0].onclick = () => {
     const todoText = document.getElementsByClassName("todo-text");
     const todoList = document.getElementsByClassName("todo-list");
     const li = document.createElement("li");
-   li.innerHTML += `
+    li.innerHTML = `
         <input type="checkbox" class="ok-check"> <span class="todo-content">${todoText[0].value}</span>
     `;
     todoList[0].appendChild(li);
